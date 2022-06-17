1658dc111e16c964e7b6ffcf2a9f2fa7786ed37d
ANAND VISHWAKARMA <anand23197@gmail.com>
2022-06-17 08:18:48 +0530

    second

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import "./navbar.scss"
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenOutlinedIcon from '@mui/icons-material/FullscreenOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlinedIcon from '@mui/icons-material/ChatBubbleOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
+
+
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className='wrapper'>
+        <div className="search">
+          <input type="text" placeholder='search..' />
+          <SearchOutlinedIcon/>
+        </div>
+        <div className="items">
+         <div className="item">
+         <LanguageOutlinedIcon className='icon'/>
+          English
+         </div>
+         <div className='item'>
+          <DarkModeOutlinedIcon className='icon'/>
+         </div>
+         <div className='item'>
+          <FullscreenOutlinedIcon className='icon'/>
+         </div>
+         <div className='item'>
+          <NotificationsNoneOutlinedIcon className='icon'/>
+          <div className='counter'>1</div>
+         </div>
+         <div className='item'>
+          <ChatBubbleOutlinedIcon className='icon'/>
+          <div className='counter'>2</div>
+         </div>
+         <div className='item'>
+          <FormatListBulletedOutlinedIcon className='icon'/>
+         </div>
+          <div className='item'>
+            <img src= 'https://bestprofilepictures.com/wp-content/uploads/2021/04/Cool-Profile-Picture.jpg'
+            alt=""
+            className='avatar'
+            />
+          </div>       
+      </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,6 +3,16 @@ import "./sidebar.scss"
 import { MdDashboard, MdAdminPanelSettings } from "react-icons/md";
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import ProductionQuantityLimitsOutlinedIcon from '@mui/icons-material/ProductionQuantityLimitsOutlined';
+import AddCardSharpIcon from '@mui/icons-material/AddCardSharp';
+import AirportShuttleSharpIcon from '@mui/icons-material/AirportShuttleSharp';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import HealthAndSafetyOutlinedIcon from '@mui/icons-material/HealthAndSafetyOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 const Sidebar = () => {
   return (
@@ -13,47 +23,65 @@ const Sidebar = () => {
       <hr/>
       <div className='center'>
         <ul>
+          <p className='title'>MAIN</p>
           <li>
-            <DashboardOutlinedIcon/>
+            <DashboardOutlinedIcon className='icon'/>
             <span>DashBoard</span>
           </li>
+          <p className='title'>LISTS</p>
           <li>
-            <AccountCircleOutlinedIcon/>
+            <AccountCircleOutlinedIcon className='icon'/>
             <span>Users</span>
           </li>
           <li>
+            <ProductionQuantityLimitsOutlinedIcon className='icon'/>
             <span>Products</span>
           </li>
           <li>
+            <AddCardSharpIcon className='icon'/>
             <span>Orders</span>
           </li>
           <li>
+            <AirportShuttleSharpIcon className='icon'/>
             <span>Delivery</span>
           </li>
+          <p className='title'>USEFUL</p>
           <li>
+            <BarChartOutlinedIcon className='icon'/>
             <span>Stats</span>
           </li>
           <li>
+            <NotificationsOutlinedIcon className='icon'/>
             <span>Notification</span>
           </li>
+          <p className='title'>SERVICE</p>
           <li>
+            <HealthAndSafetyOutlinedIcon className='icon'/>
             <span>System Health</span>
           </li>
           <li>
+            <PsychologyOutlinedIcon className='icon'/>
             <span>Logs</span>
           </li>
           <li>
+            <SettingsOutlinedIcon className='icon'/>
             <span>Settings</span>
           </li>
+          <p className='title'>USER</p>
           <li>
+            <AccountBoxOutlinedIcon className='icon'/>
             <span>Profile</span>
           </li>
           <li>
+            <LogoutOutlinedIcon className='icon'/>
             <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className='bottom'>color Options</div>
+      <div className='bottom'>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   )
 }
